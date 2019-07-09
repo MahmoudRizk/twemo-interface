2c988a13c31df24dc4b081f8ca4a56dae48764ea
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2019-07-09 21:33:07 +0200

    Updates

---
+++ precache-manifest.babf8e718e13760999e94d37e7da4257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf7367d9bb4811b7c5ccbffea311ab7",
+    "url": "/twemo-interface/index.html"
+  },
+  {
+    "revision": "b3057cb024231d34277f",
+    "url": "/twemo-interface/static/css/main.7fc9de20.chunk.css"
+  },
+  {
+    "revision": "d0c7dde590bffac43b12",
+    "url": "/twemo-interface/static/js/2.1377fdb4.chunk.js"
+  },
+  {
+    "revision": "b3057cb024231d34277f",
+    "url": "/twemo-interface/static/js/main.1333c5f2.chunk.js"
+  },
+  {
+    "revision": "657a1c53420c2699d46d",
+    "url": "/twemo-interface/static/js/runtime~main.a5154452.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twemo-interface/static/media/logo.5d5d9eef.svg"
+  }
+]);
